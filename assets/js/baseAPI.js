61412de18572f4fd5a4dffde387d9422bab9e614
smallccai <1760006822@qq.com>
2022-10-16 10:15:13 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,18 @@
 $.ajaxPrefilter(function(option){
+    //拼接根路径
     option.url='http://www.liulongbin.top:3007'+option.url
+    //统一为有权限的访问设置请求头
+    if(option.url.indexOf('/my/')!==-1){
+        option.headers = {
+            Authorization:localStorage.getItem('token')||''
+        }
+    }
+    //统一设置访问客户权限的回调函数
+    option.complete = function (res) {
+        console.log(res)
+        if (res.responseJSON.status === 1 || res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = './login.html'
+        }
+    }
 })
